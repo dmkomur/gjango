2f89a76cf3071ff6d41f01721e61b3d89d0d36a0
dmkomur <125804681+dmkomur@users.noreply.github.com>
2025-08-14 21:54:08 +0200

    add templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    ordering = ["-published"]  # эквивалент .order_by("-published")
+    template_name = "blog/index.html"  # иначе искал бы blog/post_list.html
+    context_object_name = "posts"  # по умолчанию object_list
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/detail.html"  # иначе blog/post_detail.html
+    context_object_name = "post"
